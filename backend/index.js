c19b322ff19dc5e08e4b09bdc463ee10ecb80b09
AhmadMohSal <ahmad.moh.sal@gmail.com>
2022-06-01 12:44:14 +0300

    createNewCategory,createNewBrand,addProduct

--- backend/index.js
+++ backend/index.js
@@ -12,11 +12,14 @@ const rolesRouter = require("./routes/roles");
 const permissionsRouter = require("./routes/permissions");
 const registerRouter = require("./routes/register");
 const loginRouter = require("./routes/login");
+const adminRouter = require("./routes/admin")
 
 app.use("/role", rolesRouter);
 app.use("/permission", permissionsRouter);
 app.use("/register", registerRouter);
 app.use("/login", loginRouter);
+app.use("/admin", adminRouter);
+
 
 const PORT = process.env.PORT || 5000;
 
