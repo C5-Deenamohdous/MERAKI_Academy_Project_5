8495f64f0f46517c5e62648aa46f9c25db516034
deenamohdous <dina.mohdous@gmail.com>
2022-06-01 12:10:56 +0300

    getAllProduct fun

--- backend/controllers/product.js
+++ backend/controllers/product.js
@@ -26,7 +26,24 @@ const getOneProductById = (req, res) => {
     });
   });
 };
-
+const getAllProduct= (req, res) => {
+    const query = `SELECT * FROM products WHERE is_deleted=0;`;
+    connection.query(query, (err, result) => {
+      if (err) {
+        res.status(500).json({
+          success: false,
+          massage: "server error",
+          err: err,
+        });
+      }
+      res.status(200).json({
+        success: true,
+        massage: "All products",
+        result: result,
+      });
+    });
+  };
 module.exports = {
   getOneProductById,
+  getAllProduct
 };
