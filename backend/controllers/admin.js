003c149835b801a0c02a4a12321e09c5e4ff8096
AhmadMohSal <ahmad.moh.sal@gmail.com>
2022-06-01 13:51:10 +0300

    deleteProductById

--- backend/controllers/admin.js
+++ backend/controllers/admin.js
@@ -2,34 +2,34 @@ const connection = require("../models/db");
 
 
 const /* A function that creates a new category. */
-createNewCategory =(req, res)=>{
-    const { categoryName } = req.body;
-    const query = `INSERT INTO categories (categoryName) VALUES (?);`;
-    const data = [categoryName];
+    createNewCategory = (req, res) => {
+        const { categoryName } = req.body;
+        const query = `INSERT INTO categories (categoryName) VALUES (?);`;
+        const data = [categoryName];
 
-    connection.query(query, data, (err, result) => {
-        if (err) {
-            res.status(500).json({
-                success: false,
-                massage: "Server error",
-                err: err,
+        connection.query(query, data, (err, result) => {
+            if (err) {
+                res.status(500).json({
+                    success: false,
+                    massage: "Server error",
+                    err: err,
+                });
+            }
+            res.status(201).json({
+                success: true,
+                massage: `${categoryName} created`,
+                result: result,
             });
-        }
-        res.status(201).json({
-            success: true,
-            massage: `${categoryName} created`,
-            result: result,
         });
-    });
-}
+    }
 
 
 /* A function that creates a new brand. */
-const createNewBrand =(req, res)=>{
+const createNewBrand = (req, res) => {
     const id = req.params.id;
     const { brandName } = req.body;
     const query = `INSERT INTO brands (brandName,category_id) VALUES (?,?);`;
-    const data = [brandName,id];
+    const data = [brandName, id];
 
     connection.query(query, data, (err, result) => {
         if (err) {
@@ -49,76 +49,116 @@ const createNewBrand =(req, res)=>{
 
 
 const /* A function that creates a new product. */
-addProduct =(req, res)=>{
-    const { title,description,productImage,price,quantity,category_id,brand_id } = req.body;
-    const query = `INSERT INTO products (title,description,productImage,price,quantity,category_id,brand_id) VALUES (?,?,?,?,?,?,?);`;
-    const data = [title,description,productImage,price,quantity,category_id,brand_id];
+    addProduct = (req, res) => {
+        const { title, description, productImage, price, quantity, category_id, brand_id } = req.body;
+        const query = `INSERT INTO products (title,description,productImage,price,quantity,category_id,brand_id) VALUES (?,?,?,?,?,?,?);`;
+        const data = [title, description, productImage, price, quantity, category_id, brand_id];
 
-    connection.query(query, data, (err, result) => {
-        if (err) {
-            res.status(500).json({
-                success: false,
-                massage: "Server error",
-                err: err,
+        connection.query(query, data, (err, result) => {
+            if (err) {
+                res.status(500).json({
+                    success: false,
+                    massage: "Server error",
+                    err: err,
+                });
+            }
+            res.status(200).json({
+                success: true,
+                massage: "product created",
+                result: result,
             });
-        }
-        res.status(200).json({
-            success: true,
-            massage: "product created",
-            result: result,
         });
-    });
-}
+    }
 const /* Getting all the users from the database. */
-getAllusers = (req, res) => {
-    const query = `SELECT * FROM users WHERE is_deleted=0;`;
-    connection.query(query, (err, result) => {
-        if (err) {
-            res.status(500).json({
-                success: false,
-                massage: "server error",
-                err: err,
+    getAllusers = (req, res) => {
+        const query = `SELECT * FROM users WHERE is_deleted=0;`;
+        connection.query(query, (err, result) => {
+            if (err) {
+                res.status(500).json({
+                    success: false,
+                    massage: "server error",
+                    err: err,
+                });
+            }
+            res.status(200).json({
+                success: true,
+                massage: "All the users",
+                result: result,
             });
-        }
-        res.status(200).json({
-            success: true,
-            massage: "All the users",
-            result: result,
         });
-    });
-};
+    };
 
 const /* Deleting the user with the given id. */
-deleteUserById = (req, res) => {
-    const id = req.params.id;
+    deleteUserById = (req, res) => {
+        const id = req.params.id;
 
-    const query = `UPDATE users SET is_deleted=1 WHERE id=?;`;
+        const query = `UPDATE users SET is_deleted=1 WHERE id=?;`;
 
-    const data = [id];
+        const data = [id];
 
-    connection.query(query, data, (err, result) => {
-        if (err) {
-            return res.status(500).json({
-                success: false,
-                massage: "Server Error",
-                err: err,
+        connection.query(query, data, (err, result) => {
+            if (err) {
+                return res.status(500).json({
+                    success: false,
+                    massage: "Server Error",
+                    err: err,
+                });
+            }
+            if (!result.changedRows) {
+                return res.status(404).json({
+                    success: false,
+                    massage: `The user: ${id} is not found`,
+                    err: err,
+                });
+            }
+            res.status(200).json({
+                success: true,
+                massage: `Succeeded to delete user with id: ${id}`,
+                result: result,
             });
-        }
-        if (!result.changedRows) {
-            return res.status(404).json({
-                success: false,
-                massage: `The user: ${id} is not found`,
-                err: err,
-            });
-        }
-        res.status(200).json({
-            success: true,
-            massage: `Succeeded to delete user with id: ${id}`,
-            result: result,
         });
-    });
-};
+    };
 
-module.exports={createNewCategory,createNewBrand,addProduct,getAllusers,deleteUserById};
+    const /* Deleting the product with the given id. */
+    deleteProductById = (req, res) => {
+        const id = req.params.id;
+
+        const query = `UPDATE products SET is_deleted=1 WHERE id=?;`;
+
+        const data = [id];
+
+        connection.query(query, data, (err, result) => {
+            if (err) {
+                return res.status(500).json({
+                    success: false,
+                    massage: "Server Error",
+                    err: err,
+                });
+            }
+            if (!result.changedRows) {
+                return res.status(404).json({
+                    success: false,
+                    massage: `The product: ${id} is not found`,
+                    err: err,
+                });
+            }
+            res.status(200).json({
+                success: true,
+                massage: `Succeeded to delete product with id: ${id}`,
+                result: result,
+            });
+        });
+    };
+
+
+
+module.exports = {
+    createNewCategory,
+    createNewBrand,
+    addProduct,
+    getAllusers,
+    deleteUserById,
+    deleteProductById,
+};
 
 
