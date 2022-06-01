acd8495550b81ab558c8aa2246e745475ea5355d
deenamohdous <dina.mohdous@gmail.com>
2022-06-01 12:39:53 +0300

    getProductByCategory fun

--- backend/routes/product.js
+++ backend/routes/product.js
@@ -2,8 +2,9 @@ const express = require("express");
 
 const productRouter = express.Router();
 
-const {  getOneProductById,getAllProduct} = require("../controllers/product");
+const {  getOneProductById,getAllProduct, getProductByCategory} = require("../controllers/product");
 
 productRouter.get("/:id", getOneProductById);
-productRouter.get("/",getAllProduct)
+productRouter.get("/",getAllProduct);
+productRouter.get("./category/:id",getProductByCategory)
 module.exports = productRouter;
