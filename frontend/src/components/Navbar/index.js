954f68f1fee38c536412464ca645a9b904f723e0
AhmadTayseer <ahmad.ty.taha@gmail.com>
2022-06-03 01:04:35 +0300

    Resolve Conflict

--- frontend/src/components/Navbar/index.js
+++ frontend/src/components/Navbar/index.js
@@ -2,22 +2,17 @@ import React, { useContext } from "react";
 import { Link } from "react-router-dom";
 
 import { useDispatch, useSelector } from "react-redux";
-import Category  from "../Category"
+import Category from "../Category";
 
 const NavBar = () => {
-
-    const dispatch = useDispatch();
-
+  const dispatch = useDispatch();
 
   return (
-
-    <div className="NavBar">NavBar</div>
-
-    <div>NavBar
-      <Category/>
+    <div className="NavBar">
+      NavBar
+      <Category />
     </div>
+  );
+};
 
-  )
-}
-
-export default NavBar
+export default NavBar;
