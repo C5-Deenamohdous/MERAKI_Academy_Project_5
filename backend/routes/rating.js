caddf5211bd1a5c5b6cf0f48f7e96afdb5dd2463
AhmadTayseer <ahmad.ty.taha@gmail.com>
2022-06-02 05:54:43 +0300

    productsRating

--- backend/routes/rating.js
+++ backend/routes/rating.js
@@ -2,9 +2,14 @@ const express = require("express");
 
 const ratingRouter = express.Router();
 //
-const { addRatingToProduct } = require("../controllers/rating");
+const {
+  addRatingToProduct,
+  getProductRating,
+} = require("../controllers/rating");
 const authentication = require("../middlewares/authentication");
 
 ratingRouter.post("/:id", authentication, addRatingToProduct);
+
+ratingRouter.get("/:id", authentication, getProductRating);
 
 module.exports = ratingRouter;
