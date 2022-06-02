7bdbe61c395ce4f9669127264870e8de2c866737
deenamohdous <dina.mohdous@gmail.com>
2022-06-02 12:06:47 +0300

    edit create reducer for product and imprt it to store

---
+++ frontend/src/components/Navbar/index.js
@@ -0,0 +1,16 @@
+import React, { useContext } from "react";
+import { Link } from "react-router-dom";
+import "./style.css";
+import { useDispatch, useSelector } from "react-redux";
+
+
+const NavBar = () => {
+    const dispatch = useDispatch();
+
+
+  return (
+    <div>NavBar</div>
+  )
+}
+
+export default NavBar
---
+++ frontend/src/components/Products/index.js
@@ -0,0 +1,35 @@
+import React, { useContext, useEffect, useState } from "react";
+import "./style.css";
+import { useSelector, useDispatch } from "react-redux";
+import axios from "axios";
+import { setProducts } from "../../redux/reducers/products";
+
+const Product = () => {
+  const dispatch = useDispatch();
+  const [message, setMessage] = useState("");
+  const { products } = useSelector((state) => {
+    return {
+      products: state.products.products,
+    };
+  });
+  const getAllProducts = async () => {
+    axios
+      .get("http://localhost:5000/product/")
+
+      .then((result) => {
+        console.log(result, "}}}}}");
+        dispatch(setProducts(result.data.result));
+        setMessage("ALL products");
+      })
+      .catch((err) => {
+        setMessage(err.response.data.message);
+      });
+  };
+  useEffect(() => {
+    getAllProducts();
+  }, []);
+
+  return <div></div>;
+};
+
+export default Product;
