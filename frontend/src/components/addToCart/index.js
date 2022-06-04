aa04fbe0ef6ff2803c89d4c0657e6f9b0a9eaed0
AhmadTayseer <ahmad.ty.taha@gmail.com>
2022-06-04 07:41:40 +0300

    Toggle AddToCart

--- frontend/src/components/addToCart/index.js
+++ frontend/src/components/addToCart/index.js
@@ -1,18 +1,41 @@
-import React, { useState } from "react";
-
-import { useSelector, useDispatch } from "react-redux";
-
+import React, { useState, useEffect } from "react";
 import axios from "axios";
-
+import { useSelector, useDispatch } from "react-redux";
 import { addToCart } from "../../redux/reducers/cart";
 
-const AddToCartButton = ({ element }) => {
-  
+const AddToCartButton = ({ productId }) => {
   const dispatch = useDispatch();
+  const { productInCart , token} = useSelector((state) => {
+    return {
+      cart: state.cart.cart,
+      productInCart: state.cart.productInCart,
+      token:state.auth.token
+    };
+  });
+  const AddToCart = (id) => { 
+    
 
-  return <button onClick={() => {
 
-  }}></button>;
+
+  }
+
+
+  console.log(productInCart, "ALL ID's inCart");
+  return (
+    <>
+      {productInCart.includes(productId) ? (
+        <button
+          onClick={() => {
+            addToCart(productId);
+          }}
+        >
+          Add
+        </button>
+      ) : (
+        <button>Remove</button>
+      )}
+    </>
+  );
 };
 
 export default AddToCartButton;
--- frontend/src/redux/reducers/cart/index.js
+++ frontend/src/redux/reducers/cart/index.js
@@ -4,17 +4,24 @@ export const cartSlice = createSlice({
   name: "cart",
   initialState: {
     cart: [],
+    productInCart: [],
   },
   reducers: {
     setCart: (state, action) => {
       state.cart = action.payload;
+      state.productInCart = state.cart.map((element) => {
+        return element.product_id;
+      });
     },
     addToCart: (state, action) => {
       state.cart = [...state.cart, ...action.payload];
     },
+    setProductInCart: (state, action) => {
+      state.productInCart = action.payload;
+    },
   },
 });
 
-export const { addToCart } = cartSlice.actions;
+export const { addToCart, setCart} = cartSlice.actions;
 
 export default cartSlice.reducer;
