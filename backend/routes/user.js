3f80c768c962a40275f97a93048ff77a2a89f1da
AhmadMohSal <ahmad.moh.sal@gmail.com>
2022-06-04 20:38:27 +0300

    UserRouter routs

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -11,12 +11,10 @@ const {
   updateUserById,
 } = require("../controllers/user");
 
-UserRouter.get("/:id", getUserById); //!
-/* UserRouter is a router that is used to handle all the user routes. */
-UserRouter.get("/users", getAllusers); //!done
-/* Deleting a user by id. */
-UserRouter.delete("/delete_user/:id", deleteUserById); //!done
-UserRouter.put("/:id", updateUserById); //!done
+UserRouter.get("/:id", getUserById);
+UserRouter.get("/users", getAllusers);
+UserRouter.delete("/:id", deleteUserById); 
+UserRouter.put("/:id", updateUserById); 
 
 
 module.exports = UserRouter;
