45229473105afa84a028d0987b7a70affc507e9e
deenamohdous <dina.mohdous@gmail.com>
2022-06-05 22:17:40 +0300

    done mail service

--- frontend/src/components/ContactUs/index.js
+++ frontend/src/components/ContactUs/index.js
@@ -1,32 +1,40 @@
-import React , {useRef} from "react";
-import emailjs from 'emailjs-com';
+import React, { useRef } from "react";
+import emailjs from "emailjs-com";
 const ContactUs = () => {
-    const form = useRef();
-  
-    const sendEmail = (e) => {
-      e.preventDefault();
-  
-      emailjs.sendForm('service_1h0nflc', 'template_0oc8ifp', form.current, '4qjKDmecaKgSq4X7L')
-        .then((result) => {
-            console.log(result.text);
-        }, (error) => {
-            console.log(error.text);
-        });
-    };
-  
-    return (
-      <form ref={form} onSubmit={sendEmail}>
-        <label>Name</label>
-        <input type="text" name="user_name" />
-        <br/>
-        <label>Email</label>
-        <input type="email" name="user_email" />
-        <br/>
-        <label>Message</label>
-        <textarea name="message" />
-        <br/>
-        <input type="submit" value="Send" />
-      </form>
-    );
+  const form = useRef();
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs
+      .sendForm(
+        "service_1h0nflc",
+        "template_0oc8ifp",
+        form.current,
+        "4qjKDmecaKgSq4X7L"
+      )
+      .then(
+        (result) => {
+          console.log(result.text);
+        },
+        (error) => {
+          console.log(error.text);
+        }
+      );
   };
-  export default ContactUs
+
+  return (
+    <form ref={form} onSubmit={sendEmail}>
+      <label>Name</label>
+      <input type="text" name="user_name" />
+      <br />
+      <label>Email</label>
+      <input type="email" name="user_email" />
+      <br />
+      <label>Message</label>
+      <textarea name="message" />
+      <br />
+      <input type="submit" value="Send" />
+    </form>
+  );
+};
+export default ContactUs;
