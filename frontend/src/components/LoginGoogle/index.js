8cec57f415f270269c0eca7bd74854c23de38625
deenamohdous <dina.mohdous@gmail.com>
2022-06-06 22:24:16 +0300

    loginWithGoogle Backend compnent

--- frontend/src/components/LoginGoogle/index.js
+++ frontend/src/components/LoginGoogle/index.js
@@ -1,11 +1,15 @@
 import React from "react";
 import { GoogleLogin } from "react-google-login";
+import { useNavigate } from "react-router-dom";
+
 const clientId =
   "171142303177-dlklu0me533t11g37ll28pjmd603vh8c.apps.googleusercontent.com";
 const LoginGoogle = () => {
+  const navigate = useNavigate();
   const onSuccess = (res) => {
     console.log(res);
     console.log("login success current user :", res);
+    navigate("/Product");
   };
   const onFailure = (res) => {
     console.log("login failed  :", res);
