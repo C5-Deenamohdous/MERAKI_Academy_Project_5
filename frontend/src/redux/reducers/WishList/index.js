6e3ea15233d16f7426f8a9eb2bf4f797a1fc826c
AhmadMohSal <ahmad.moh.sal@gmail.com>
2022-06-06 11:48:24 +0300

    WishList slice

---
+++ frontend/src/redux/reducers/WishList/index.js
@@ -0,0 +1,39 @@
+import { createSlice } from "@reduxjs/toolkit";
+export const WishlistSlice = createSlice({
+    name: "Wishlist",
+    initialState: {
+      Wishlist: [],
+      productInWishlist: [],
+    },
+    reducers: {
+      setWishlist: (state, action) => {
+        state.Wishlist = action.payload;
+        state.productInWishlist = state.Wishlist.map((element) => {
+          return element.product_id;
+        });
+      },
+      addToWishlist: (state, action) => {
+        state.Wishlist = [...state.Wishlist, action.payload];
+        state.productInWishlist = state.Wishlist.map((element) => {
+          return element.product_id;
+        });
+      },
+      removeFromWishlist: (state, action) => {
+        state.Wishlist = state.Wishlist.filter((element) => {
+          return element.product_id !== action.payload.product_id;
+        });
+        state.productInWishlist = state.productInWishlist.filter((element) => {
+          return element !== action.payload.product_id;
+        });
+        state.subTotal = state.Wishlist.reduce((total, element) => {
+          return total + element.price * element.quantityInWishlist;
+        }, 0);
+      },
+    },
+  });
+  
+  export const { addToWishlist, setWishlist, removeFromWishlist } =
+    WishlistSlice.actions;
+  
+  export default WishlistSlice.reducer;
+  
