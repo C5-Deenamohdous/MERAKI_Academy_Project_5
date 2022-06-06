8903aa9f22b51fefb2ce19f27feace41a066a432
deenamohdous <dina.mohdous@gmail.com>
2022-06-06 23:59:30 +0300

    frontend google login

--- backend/controllers/loginGoogle.js
+++ backend/controllers/loginGoogle.js
@@ -1,20 +1,24 @@
 const connection = require("../models/db");
-
-
-const loginGoogle = (req,res) => {
+const jwt = require("jsonwebtoken");
+const loginGoogle =  (req,res) => {
+   
  const email=req.body.email;
+console.log(email,"emailllllllllll");
  const query="SELECT * FROM users WHERE email = ? ;"; 
- const data=[email.toLowerCase()];
- connection.query( query, data, (err,result) => {
+ const data=[email];
+ connection.query( query, data,(err,result) => {
+     console.log(result,"rresulttttttt in query ");
+     if(err){console.log(err);}
       if (result.length){
 try{
+   console.log("try backkeeeeeeee");
     const payload = {
         userId: result[0].id,
         role: result[0].role_id,
       };
       const options = { expiresIn: "180m" };
       const secret = process.env.SECRET;
-      const token = await jwt.sign(payload, secret, options);
+      const token =jwt.sign(payload, secret, options);
 
       return res.status(200).json({
         success: true,
@@ -24,6 +28,7 @@ try{
         role_id: result[0].role_id,
       });
 }catch(err){
+    console.log("login error backkeeenndddd");
     res.status(500).json({
         success: false,
         message: `Server Error`,
