ffcae4d5f0d85042c2677212b3a5c3eeeba7ac8e
deenamohdous <dina.mohdous@gmail.com>
2022-06-08 00:22:07 +0300

    start shop  now button

--- frontend/src/components/Home/index.js
+++ frontend/src/components/Home/index.js
@@ -1,13 +1,18 @@
-
+import { useNavigate } from "react-router-dom";
 import React from 'react'
 import Category from "../Category";
 import Brand from "../Brand";
+import "./style.css";
 const Home = () => {
+    const navigate=useNavigate();
   return (
     <div>
       
           <Category />
           <Brand />
+          <button className="shopNowButton" onClick={()=>{
+navigate("/Product")
+          }}>Shop Now</button>
     </div>
   )
 }
