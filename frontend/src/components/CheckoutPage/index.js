d9e54a430cedc7659e8584b047b5ad5741169848
AhmadTayseer <ahmad.ty.taha@gmail.com>
2022-06-15 00:12:35 +0300

    CheckOut Style

--- frontend/src/components/CheckoutPage/index.js
+++ frontend/src/components/CheckoutPage/index.js
@@ -83,197 +83,215 @@ const CheckOutPage = () => {
 
   return (
     <div className="CheckOuContainer">
-      {cart &&
-        cart.map((element) => {
-          return (
-            <div className="CheckOutPage">
-              <div className="UserShippingInfo-L">
-                <div className="ContactInfo">
-                  <span>Contanct Information</span>
-                  <div className="Input-Label">
-                    <label>Email</label>
-                    <input defaultValue={element.email} />
-                  </div>
-                </div>
-                <div className="shippingAdress">
-                  <span>Shipping Information</span>
-                  <div className="Input-Label">
-                    <label>Adress</label>
-                    <input defaultValue={"Adress For User After Added "} />
-                  </div>
-                </div>
-                <div className="FirstAndLastName">
-                  <div className="Input-Label F">
-                    <label>FirstName</label>
-                    <input defaultValue={firstName} />
-                  </div>
-                  <div className="Input-Label F">
-                    <label>Last Name</label>
-                    <input defaultValue={lastName} />
-                  </div>
-                </div>
-                <div className="Input-Label Ph">
-                  <label>Phone Number</label>
-                  <input defaultValue={phoneNumber} />
-                </div>
-                <div className="B-Contaier">
-                  <div>
-                    <p
-                      onClick={() => {
-                        navigate("/cart");
-                      }}
-                    >
-                      Back To Cart
-                    </p>
-                  </div>
-                </div>
-                <div className="PaymentMethod">
-                  <div className="selectContainer">
-                    <select
-                      onChange={(e) => {
-                        if (e.target.value == 1) {
-                          setMethod1(true);
-                          setMethod2(false);
-                        }
-                        if (e.target.value == 2) {
-                          setMethod2(true);
-                          setMethod1(false);
-                        }
-                      }}
-                    >
-                      <option disabled selected>
-                        Payment Method
-                      </option>
-                      <option value="1">Cash On Delivery</option>
-                      <option value="2">Online Payment</option>
-                    </select>
-                  </div>
-                </div>
-                {method2 ? (
-                  <div className="PaypalInCheckoutPage">
-                    <Payment />
-                  </div>
-                ) : (
-                  ""
-                )}
-                {method1 ? (
-                  <div className="CashPayment">
-                    <button
-                      onClick={() => {
-                        setIsOpen1(true);
-                      }}
-                    >
-                      Checkout
-                    </button>
-                  </div>
-                ) : (
-                  ""
-                )}
-              </div>
-              {/*  */}
-              <div className="OrdersInfo-R"></div>
+      <div className="CheckOutPage">
+        <div className="UserShippingInfo-L">
+          <div className="ContactInfo">
+            <span>Contanct Information</span>
+            <div className="Input-Label">
+              <label>Email</label>
+              <input defaultValue={email} />
             </div>
-          );
-        })}
-      {cart.length == 0 && method2 ? (
-        <Modal
-          ariaHideApp={false}
-          className={"MessageAfterPayment"}
-          isOpen={isOpen}
-          onRequestClose={() => setIsOpen(false)}
-        >
-          <div className="messageContainer">
-            <div className="ceneteInsidePopUp">
-              <span>
-                {firstName} {lastName} ,,
-              </span>
-            </div>
-            <div className="ceneteInsidePopUp">
-              <span>thank you for choosing us</span>
-            </div>
-            <div className="ceneteInsidePopUp">
-              <span>You Can Check Your Order Status From Your Profile</span>
-            </div>
-
-            <button
-              className="BackTo"
-              onClick={() => {
-                setIsOpen(false);
-                navigate("/");
-              }}
-            >
-              Back To Shopping
-            </button>
           </div>
-        </Modal>
-      ) : (
-        ""
-      )}
-
-      <Modal
-        ariaHideApp={false}
-        className={"ConfirmationMessagePopUp"}
-        isOpen={isOpen1}
-        onRequestClose={() => setIsOpen1(false)}
-      >
-        <div className="Cont-Confi">
-          <div>
-            <p>Are you sure From Your informations ? </p>
-            <div className="BtnsConfirmation">
-              <button
-                className="CancelBtn"
+          <div className="shippingAdress">
+            <span>Shipping Information</span>
+            <div className="Input-Label">
+              <label>Adress</label>
+              <input defaultValue={"Adress For User After Added "} />
+            </div>
+          </div>
+          <div className="FirstAndLastName">
+            <div className="Input-Label F">
+              <label>FirstName</label>
+              <input defaultValue={firstName} />
+            </div>
+            <div className="Input-Label F">
+              <label>Last Name</label>
+              <input defaultValue={lastName} />
+            </div>
+          </div>
+          <div className="Input-Label Ph">
+            <label>Phone Number</label>
+            <input defaultValue={phoneNumber} />
+          </div>
+          <div className="B-Contaier">
+            <div>
+              <p
                 onClick={() => {
-                  setIsOpen1(false);
+                  navigate("/cart");
                 }}
               >
-                Cancel
-              </button>
-              <button
-                className="Confirm"
-                onClick={() => {
-                  addToOrders();
+                Back To Cart
+              </p>
+            </div>
+          </div>
+          <div className="PaymentMethod">
+            <div className="selectContainer">
+              <select
+                onChange={(e) => {
+                  if (e.target.value == 1) {
+                    setMethod1(true);
+                    setMethod2(false);
+                  }
+                  if (e.target.value == 2) {
+                    setMethod2(true);
+                    setMethod1(false);
+                  }
                 }}
               >
-                Confirm Order
+                <option disabled selected>
+                  Payment Method
+                </option>
+                <option value="1">Cash On Delivery</option>
+                <option value="2">Online Payment</option>
+              </select>
+            </div>
+          </div>
+          {method2 ? (
+            <div className="PaypalInCheckoutPage">
+              <Payment />
+            </div>
+          ) : (
+            ""
+          )}
+          {method1 ? (
+            <div className="CashPayment">
+              <button
+                onClick={() => {
+                  setIsOpen1(true);
+                }}
+              >
+                Checkout
               </button>
             </div>
+          ) : (
+            ""
+          )}
+        </div>
+        <div className="OrdersInfo-R">
+          {cart &&
+            cart.map((element) => {
+              return (
+                <>
+                  <div className="CheckOutSideCart">
+                    <div className="checkOutSidCartImage">
+                      <img src={element.productImage} />
+                    </div>
+                    <div className="Title_PriceInCheckOut">
+                      <span>{element.title}</span>
+                      <span className="QuanInCheckOut">
+                        {element.quantityInCart} x {element.price}
+                      </span>
+                    </div>
+                  </div>
+                </>
+              );
+            })}
+          {cart.length == 0 && method2 ? (
+            <Modal
+              ariaHideApp={false}
+              className={"MessageAfterPayment"}
+              isOpen={isOpen}
+              onRequestClose={() => setIsOpen(false)}
+            >
+              <div className="messageContainer">
+                <div className="ceneteInsidePopUp">
+                  <span>
+                    {firstName} {lastName} ,,
+                  </span>
+                </div>
+                <div className="ceneteInsidePopUp">
+                  <span>thank you for choosing us</span>
+                </div>
+                <div className="ceneteInsidePopUp">
+                  <span>You Can Check Your Order Status From Your Profile</span>
+                </div>
+
+                <button
+                  className="BackTo"
+                  onClick={() => {
+                    setIsOpen(false);
+                    navigate("/");
+                  }}
+                >
+                  Back To Shopping
+                </button>
+              </div>
+            </Modal>
+          ) : (
+            ""
+          )}
+
+          <Modal
+            ariaHideApp={false}
+            className={"ConfirmationMessagePopUp"}
+            isOpen={isOpen1}
+            onRequestClose={() => setIsOpen1(false)}
+          >
+            <div className="Cont-Confi">
+              <div>
+                <p>Are you sure From Your informations ? </p>
+                <div className="BtnsConfirmation">
+                  <button
+                    className="CancelBtn"
+                    onClick={() => {
+                      setIsOpen1(false);
+                    }}
+                  >
+                    Cancel
+                  </button>
+                  <button
+                    className="Confirm"
+                    onClick={() => {
+                      addToOrders();
+                    }}
+                  >
+                    Confirm Order
+                  </button>
+                </div>
+              </div>
+            </div>
+          </Modal>
+          {isConfirm && method1 ? (
+            <Modal
+              ariaHideApp={false}
+              className={"MessageAfterPayment"}
+              isOpen={isOpen}
+              onRequestClose={() => setIsOpen(false)}
+            >
+              <div className="messageContainer">
+                <div className="ceneteInsidePopUp">
+                  <span>
+                    {firstName} {lastName} ,,
+                  </span>
+                </div>
+                <div className="ceneteInsidePopUp">
+                  <span>thank you for choosing us</span>
+                </div>
+                <div className="ceneteInsidePopUp">
+                  <span>You Can Check Your Order Status From Your Profile</span>
+                </div>
+
+                <button
+                  className="BackTo"
+                  onClick={() => {
+                    setIsOpen(false);
+                    navigate("/");
+                  }}
+                >
+                  Back To Shopping
+                </button>
+              </div>
+            </Modal>
+          ) : (
+            ""
+          )}
+          <div className="Total">
+            <span className="T_">Total</span>
+            <span className="Num">${subTotal}</span>
           </div>
         </div>
-      </Modal>
-      {isConfirm && method1 ? (
-        <Modal
-          ariaHideApp={false}
-          className={"MessageAfterPayment"}
-          isOpen={isOpen}
-          onRequestClose={() => setIsOpen(false)}
-        >
-          <div className="messageContainer">
-            <div className="ceneteInsidePopUp">
-              <span>
-                {firstName} {lastName} ,,
-              </span>
-            </div>
-            <div className="ceneteInsidePopUp">
-              <span>thank you for choosing us</span>
-            </div>
-            <div className="ceneteInsidePopUp">
-              <span>You Can Check Your Order Status From Your Profile</span>
-            </div>
-
-            <button
-              className="BackTo"
-              onClick={() => {
-                setIsOpen(false);
-                navigate("/");
-              }}
-            >
-              Back To Shopping
-            </button>
-          </div>
-        </Modal>
-      ) : (
-        ""
-      )}
+        ;
+      </div>
     </div>
   );
 };
