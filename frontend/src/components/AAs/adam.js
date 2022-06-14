d976be1dff8417d5f4bca6e0092a7f69688d409b
AhmadMohSal <ahmad.moh.sal@gmail.com>
2022-06-15 02:36:46 +0300

    new analysiess pic

--- frontend/src/components/AAs/adam.js
+++ frontend/src/components/AAs/adam.js
@@ -14,14 +14,27 @@ export default function Adam() {
     
     const data = [
 
-        { x: 0, y: 8 },
-        { x: 1, y: 5 },
-        { x: 2, y: 4 },
-        { x: 3, y: 9 },
-        { x: 4, y: 1 },
-        { x: 5, y: 7 },
-        { x: 6, y: 6 },
-        { x: 7, y: 20 },
+        { x: 0, y: 2 },
+        { x: 1, y: 4 },
+        { x: 2, y: 6 },
+        { x: 3, y: 8 },
+        { x: 4, y: 10 },
+        { x: 5, y: 12 },
+        { x: 6, y: 14 },
+        { x: 7, y: 16 },
+
+        
+    ];
+    const data2 = [
+
+        { x: 0, y: 50 },
+        { x: 1, y: 40 },
+        { x: 2, y: 60 },
+        { x: 3, y: 80 },
+        { x: 4, y: 30 },
+        { x: 5, y: 20 },
+        { x: 6, y: 40 },
+        { x: 7, y: 60 },
 
         
     ];
@@ -31,6 +44,7 @@ export default function Adam() {
         { x: 0, y: 1, label: 'cool Dog friend', xOffset: 5, rotation: 34 }
     ]
     return (
+        <div>
         <div>
             <XYPlot width={300} height={300}>
                 <VerticalGridLines />
@@ -44,6 +58,33 @@ export default function Adam() {
                 />
             </XYPlot>
         </div>
+<div>
+            <XYPlot width={300} height={300}>
+                <VerticalGridLines />
+                <HorizontalGridLines />
+                <XAxis />
+                <YAxis />
+                <AreaSeries
+                    className="area-series-example"
+                    curve="curveNatural"
+                    data={myData}
+                />
+            </XYPlot>
+</div>
+<div>
+            <XYPlot width={300} height={300}>
+                <VerticalGridLines />
+                <HorizontalGridLines />
+                <XAxis />
+                <YAxis />
+                <AreaSeries
+                    className="area-series-example"
+                    curve="curveNatural"
+                    data={data2}
+                />
+            </XYPlot>
+</div>
+</div>
     );
 
 }
