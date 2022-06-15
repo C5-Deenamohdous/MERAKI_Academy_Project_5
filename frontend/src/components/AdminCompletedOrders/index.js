c2e8b27fe3751c42d78be591c92583f58411e913
AhmadTayseer <ahmad.ty.taha@gmail.com>
2022-06-15 08:44:55 +0300

    Modified Orders Reducer

--- frontend/src/components/AdminCompletedOrders/index.js
+++ frontend/src/components/AdminCompletedOrders/index.js
@@ -29,29 +29,38 @@ const AdminCompletedOrders = () => {
       {completedOrders &&
         completedOrders.map((element) => {
           return (
-            <div
-            className="One-Order"
-           
-            >
-            <table className="ordersTable">
-            <tr  className={element.orderStatus?"completedOrdersColor" : "unCompletedOrdersColor" }>
-                <th  onClick={() => {
-              navigate(`/admin/order_details/${element.id}`);
-            }}
-             className="idOfOrder">{element.id}</th>
-                <th className="idOfOrder">{element.orderDate.substring(0,10)}</th>
-                {/* <th className="idOfOrder">
+            <div className="One-Order">
+              <table className="ordersTable">
+                <tr
+                  className={
+                    element.orderStatus
+                      ? "completedOrdersColor"
+                      : "unCompletedOrdersColor"
+                  }
+                >
+                  <th
+                    onClick={() => {
+                      navigate(`/admin/order_details/${element.id}`);
+                    }}
+                    className="idOfOrder"
+                  >
+                    {element.id}
+                  </th>
+                  <th className="idOfOrder">
+                    {element.orderDate.substring(0, 10)}
+                  </th>
+                  {/* <th className="idOfOrder">
                   {element.orderStatus ? "Completed" : "Un Completed"}
                 </th> */}
-                <th className="idOfOrder"><OrderStatus
-              order_id={element.id}
-              orderStatus={element.orderStatus}
-            /></th>
-              </tr>
-            </table>
-            
-            
-            </div> 
+                  <th className="idOfOrder">
+                    <OrderStatus
+                      order_id={element.id}
+                      orderStatus={element.orderStatus}
+                    />
+                  </th>
+                </tr>
+              </table>
+            </div>
           );
         })}
     </>
