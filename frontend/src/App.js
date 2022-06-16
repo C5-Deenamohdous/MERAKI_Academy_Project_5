bff43638bf3389a559881cfa61409d155d2a105b
AhmadTayseer <ahmad.ty.taha@gmail.com>
2022-06-16 14:42:39 +0300

    Add New

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -82,6 +82,7 @@ const App = () => {
           element={<AdminUserUnCompletedOrders />}
         />
         <Route path="/checkout" element={<CheckOutPage />} />
+        
       </Routes>
     </div>
   );
