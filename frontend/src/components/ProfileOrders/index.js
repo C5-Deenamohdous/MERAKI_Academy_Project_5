9cf9ba5c02aa4879cf409b99a764caec91a676a7
AhmadTayseer <ahmad.ty.taha@gmail.com>
2022-06-16 04:42:05 +0300

    ProfileDetails Order

--- frontend/src/components/ProfileOrders/index.js
+++ frontend/src/components/ProfileOrders/index.js
@@ -8,12 +8,12 @@ const ProfileOrders = ({ order_id }) => {
   const [isOpen, setIsOpen] = useState(false);
   const [details, setDetails] = useState("");
   const getAllDetails = () => {
-    console.log("AAAAAAAAAAA", order_id);
+    // console.log("AAAAAAAAAAA", order_id);
     axios
       .get(`http://localhost:5000/order/one_order/${order_id}`)
       .then((result) => {
         setDetails(result.data.result);
-        console.log("AAAAAAAAAAA", result);
+        // console.log("AAAAAAAAAAA", result);
       })
       .catch((err) => {
         console.log(err);
@@ -25,7 +25,8 @@ const ProfileOrders = ({ order_id }) => {
 
   return (
     <>
-      <span className="INFO_ORDERS_PROFILE"
+      <span
+        className=""
         onClick={() => {
           setIsOpen(true);
         }}
@@ -35,48 +36,74 @@ const ProfileOrders = ({ order_id }) => {
 
       <Modal
         ariaHideApp={false}
-        className={"OrderDetailsPopUp"}
+        className={"Profile-ContainerForTable"}
         isOpen={isOpen}
         onRequestClose={() => setIsOpen(false)}
       >
         {/* <div className="Center-Container"> */}
-        <div className="Col-Container ProfileCol">
-          <div className="OneOrderDetail ProfileOneOrder">
-            <div className="Num">#Item</div>
-            <div className="Details">Order Detail</div>
-            <div className="Date">Order Date</div>
-            <div className="Price">Price</div>
-            <div className="Total">Total</div>
-          </div>
+        <div className="Col-Cont-P">
+          <table className="Detail-T-Profile">
+            <tr className="TR-PROFILE">
+              <th>Product</th>
+              <th>Product Details</th>
+              <th>Order Date</th>
+              <th>Price</th>
+              <th>Total</th>
+            </tr>
+            {details &&
+              details.map((element) => {
+                return (
+                  <tr className="TR-PROFILE">
+                    <td className="T-CT">
+                      <div className="NameAndImg-T">
+                        <div className="ImgContainer-T">
+                          <img src={element.productImage} />
+                        </div>
+                        <span>{element.title}</span>
+                      </div>
+                    </td>
+                    <td>{element.description}</td>
+                    <td>{element.orderDate.substring(0, 10)}</td>
+                    <td>
+                      {element.quantityInCart}x{element.price}
+                    </td>
+                    <td> {element.quantityInCart * element.price}</td>
+                  </tr>
+                );
+              })}
+          </table>
 
-          {details &&
-            details.map((element, i) => {
-              return (
-                <div className="OneOrderDetail ProfileOneOrder">
-                  <div className="Num">{i + 1}</div>
-                  <div className="Details">
-                    <div className="img-Container">
-                      <img src={element.productImage} />
-                    </div>
-                    <div className="titleAndDesc">
-                      <p>{element.title}</p>
-                      <p>{element.description}</p>
-                    </div>
-                  </div>
-                  <div className="Date">{element.orderDate}</div>
-                  <div className="Price">
-                    {element.quantityInCart}x{element.price}
-                  </div>
-                  <div className="Total">
-                    {element.quantityInCart * element.price}
-                  </div>
-                </div>
-              );
-            })}
         </div>
+        {/* </div> */}
       </Modal>
     </>
   );
 };
 
 export default ProfileOrders;
+
+
+// {details &&
+//   details.map((element, i) => {
+//     return (
+//       <div className="OneOrderDetail ProfileOneOrder">
+//         <div className="Num">{i + 1}</div>
+//         <div className="Details">
+//           <div className="img-Container">
+//             <img src={element.productImage} />
+//           </div>
+//           <div className="titleAndDesc">
+//             <p>{element.title}</p>
+//             <p>{element.description}</p>
+//           </div>
+//         </div>
+//         <div className="Date">{element.orderDate}</div>
+//         <div className="Price">
+//           {element.quantityInCart}x{element.price}
+//         </div>
+//         <div className="Total">
+//           {element.quantityInCart * element.price}
+//         </div>
+//       </div>
+//     );
+//   })}
