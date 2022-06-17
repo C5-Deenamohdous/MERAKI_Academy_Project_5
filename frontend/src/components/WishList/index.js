f041963770a3b580ecf1e6c86d7ce289c1c1d1d1
AhmadTayseer <ahmad.ty.taha@gmail.com>
2022-06-17 21:24:26 +0300

    AA

--- frontend/src/components/WishList/index.js
+++ frontend/src/components/WishList/index.js
@@ -3,62 +3,63 @@ import React, { useEffect, useState } from "react";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
 import { useSelector, useDispatch } from "react-redux";
-import { addToWishlist,setWishlist, removeFromWishlist } from "../../redux/reducers/WishList";
- import AddToCartButton from "../AddToCart";
+import {
+  addToWishlist,
+  setWishlist,
+  removeFromWishlist,
+} from "../../redux/reducers/WishList";
+import AddToCartButton from "../AddToCart";
 import AddToWishlistButton from "../../components/addToWishlistButton";
 
-
 const WishlistSection = () => {
-    const dispatch = useDispatch();
-    const { Wishlist, token, userId, subTotal } = useSelector((state) => {
-      return {
-        Wishlist: state.Wishlist.Wishlist,
-        token: state.auth.token,
-        userId: state.auth.userId,
-        subTotal: state.Wishlist.subTotal,
-      };
-    });
-    const getProductInWishlist = () => {
-      axios
-        .get(`https://infintyzone.herokuapp.com/Wishlist`, {
-          headers: {
-            Authorization: `Bearer ${token}`,
-          },
-        })
-        .then((result) => {
-          console.log(result, `WishlistFORUSER`);
-          dispatch(setWishlist(result.data.result));
-          // subTotalCalculate(result.data.result);
-        })
-        .catch((err) => {
-          console.log(err, `ERROR IN USER Wishlist`);
-        });
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+  const { Wishlist, token, userId, subTotal } = useSelector((state) => {
+    return {
+      Wishlist: state.Wishlist.Wishlist,
+      token: state.auth.token,
+      userId: state.auth.userId,
+      subTotal: state.Wishlist.subTotal,
     };
+  });
+  const getProductInWishlist = () => {
+    axios
+      .get(`https://infintyzone.herokuapp.com/Wishlist`, {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      })
+      .then((result) => {
+        console.log(result, `WishlistFORUSER`);
+        dispatch(setWishlist(result.data.result));
+        // subTotalCalculate(result.data.result);
+      })
+      .catch((err) => {
+        console.log(err, `ERROR IN USER Wishlist`);
+      });
+  };
 
-  
-    const deleteFromWishlist = (id) => {
-      axios
-        .delete(`https://infintyzone.herokuapp.com/Wishlist/${id}`, {
-          headers: {
-            Authorization: `Bearer ${token}`,
-          },
-        })
-        .then((result) => {
-          dispatch(removeFromWishlist({ product_id: id }));
-        })
-        .catch((err) => {
-          console.log(err, "ERR DELETE FROM Wishlist");
-        });
-    };
-  
-    useEffect(() => {
-      getProductInWishlist();
-    }, []);
-  
-    return (
+  const deleteFromWishlist = (id) => {
+    axios
+      .delete(`https://infintyzone.herokuapp.com/Wishlist/${id}`, {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      })
+      .then((result) => {
+        dispatch(removeFromWishlist({ product_id: id }));
+      })
+      .catch((err) => {
+        console.log(err, "ERR DELETE FROM Wishlist");
+      });
+  };
 
+  useEffect(() => {
+    getProductInWishlist();
+  }, []);
 
-      <div className="WishlistContainer">
+  return (
+    <div className="WishlistContainer">
       <div className="Header-Container">
         <h1>Your Shopping Wishlist</h1>
       </div>
@@ -80,7 +81,7 @@ const WishlistSection = () => {
                 </td>
                 <td className="T-title">{element.title}</td>
                 <td className="T-price">{element.price}</td>
-          
+
                 <td className="T-icon">
                   <AddToWishlistButton productId={element.product_id} />
                   <AddToCartButton productId={element.product_id} />
@@ -90,12 +91,17 @@ const WishlistSection = () => {
           })}
       </table>
       <div className="TotalForWishlist">
-        <button className="Continue">CONTINUE SHOPPING</button>
+        <button
+          className="Continue"
+          onClick={() => {
+            navigate("/");
+          }}
+        >
+          CONTINUE SHOPPING
+        </button>
       </div>
-  
     </div>
-    
-    );
-  };
-  
-  export default WishlistSection;
+  );
+};
+
+export default WishlistSection;
