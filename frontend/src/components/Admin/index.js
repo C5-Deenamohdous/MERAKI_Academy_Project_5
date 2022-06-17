964e6e206ec21f069cdc84402c35d25e4d8c3dd0
AhmadTayseer <ahmad.ty.taha@gmail.com>
2022-06-17 08:06:01 +0300

    Not Found Page , and unauthorized

--- frontend/src/components/Admin/index.js
+++ frontend/src/components/Admin/index.js
@@ -5,17 +5,31 @@ import axios from "axios";
 
 import { setAllUsers } from "../../redux/reducers/admin";
 import LeftSideBar from "../LeftBar";
-import AdminNavBar from "../AdminNavBar"
+import AdminNavBar from "../AdminNavBar";
 const AdminPanel = () => {
+  const { role_id, isLoggedIn } = useSelector((state) => {
+    return {
+      role_id: state.auth.role_id,
+      isLoggedIn: state.auth.isLoggedIn,
+    };
+  });
+
   return (
     <>
-    <div className="AdminPanelContainer1" >
-        <AdminNavBar/>
-    </div>
-    <div className="AdminPanelContainer">
-    
-        <LeftSideBar />
-    </div>
+      {role_id == 4 && isLoggedIn ? (
+        <>
+          <div className="AdminPanelContainer1">
+            <AdminNavBar />
+          </div>
+          <div className="AdminPanelContainer">
+            <LeftSideBar />
+          </div>
+        </>
+      ) : (
+        <div className="AUTHIMAGE">
+          <img src="https://i.pinimg.com/originals/94/f1/dc/94f1dc2bad6923a6d36927f5766abf87.png" />
+        </div>
+      )}
     </>
   );
 };
